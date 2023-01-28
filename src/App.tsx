a57cc1a569ace6499d5a68e4a81c995e1a2e99c6
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2023-01-28 02:00:45 +0100

    Added footer and main page title

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ import Game from './components/Game';
 function App() {
   return (
     <div className='App'>
-      React Minesweeper
-
-      <Game />
+      <div className='container mx-auto p-5'>
+        <div className='flex flex-col items-center'>
+          <div className='title text-center mb-5'>
+            <h1 className='text-3xl font-semibold'>ReactSweeper</h1>
+            <h3 className='text-lg'>The famous Minesweeper game made with React and Typescript.</h3>
+          </div>
+          <Game />
+        </div>
+      </div>
     </div>
   );
 }
