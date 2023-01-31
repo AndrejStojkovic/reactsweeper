972cb90f07a197742e5e31d4c3eac1384db8569f
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2023-01-31 01:40:16 +0100

    Sort of functional board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -9,16 +9,23 @@ type BoardProps = {
 
 const Board = ({difficulty} : BoardProps)  => {
   const cfg = config[difficulty as Difficulty];
-  let board = new Array(cfg.width).fill(0).map(() => new Array(cfg.height).fill(1));
+  const [board, setBoard] = useState<Cell[][]>();
 
   useEffect(() => {
+    let newBoard: Cell[][] = [];
+
+    for(var s = 0; s < cfg.width; s++) {
+      newBoard.push([]);
+    }
+
     for(var i = 0; i < cfg.width; i++) {
       for(var j = 0; j < cfg.height; j++) {
-        board[i][j] = new Cell({
+        newBoard[i][j] = new Cell({
           isOpened: false,
           type: 'empty',
           value: -1,
         });
+        console.log(newBoard[i][j]);
       }
     }
 
@@ -29,7 +36,7 @@ const Board = ({difficulty} : BoardProps)  => {
       // Set bomb to random coordinates of map
       let x: number = randomIntFromInterval(0, cfg.width - 1);
       let y: number = randomIntFromInterval(0, cfg.height - 1);
-      board[x][y] = new Cell({
+      newBoard[x][y] = new Cell({
         isOpened: false,
         type: 'mine',
         value: -1,
@@ -38,30 +45,61 @@ const Board = ({difficulty} : BoardProps)  => {
     }
 
     // Set empty cells
+    for(var i = 0; i < cfg.width; i++) {
+      for(var j = 0; j < cfg.height; j++) {
+        if(newBoard[i][j].props.type !== 'mine') {
+          newBoard[i][j] = new Cell({
+            isOpened: false,
+            type: 'empty',
+            value: countMines(newBoard, i, j, cfg.width, cfg.height)
+          });
+        }
+      }
+    }
+
+    // set state
+    setBoard(newBoard);
   }, [cfg]);
 
   return (
     <div>Board <br />
       <div>
         {cfg.width} - {cfg.height} - {cfg.mines}
-        <div className='board flex flex-col'>
-        {board.map((cells, i) => {
-          return (
-            <div key={i} className='row flex flex-row'>
-              {cells.map((subCells, j) => {
-                return (
-                <div key={j} className='flex items-center justify-center w-6 h-6 bg-gray-200 border-1 border-gray-400 text-sm font-semibold'>
-                  <span></span>
-                </div>
-                )
-              })}
-            </div>
-          )
-        })}
+        <div className='board flex flex-col border-1 border-gray-600'>
+          {board?.map((cells, i) => {
+            return (
+              <div key={i} className='row flex flex-row'>
+                {cells.map((subCells, j) => {
+                  return (
+                  <div key={j} className='flex items-center justify-center w-6 h-6 bg-gray-100 border-1 border-gray-400 text-sm font-semibold'>
+                    <span>{subCells.props?.type === 'mine' ? 'M' : subCells.props?.value}</span>
+                  </div>
+                  )
+                })}
+              </div>
+            )
+          })}
         </div>
       </div>
     </div>
   )
+}
+
+function countMines(board: Cell[][], i: number, j: number, width: number, height: number) {
+  let ct = 0;
+  for(var x = i - 1; x < i + 1; x++) {
+    for(var y = j - 1; y < j + 1; y++) {
+      if(isValid(x, y, width, height) && board[x][y].props.type === 'mine') {
+        ct++;
+      }
+    }
+  }
+  console.log(ct);
+  return ct;
+}
+
+function isValid(i: number, j: number, width: number, height: number) {
+  return i >= 0 && i < width && j >= 0 && j < height ? 1 : 0;
 }
 
 export default Board;
--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -12,14 +12,6 @@ class Cell extends React.Component<CellState> {
     type: 'empty',
     value: -1
   }
-
-  render() {
-    return (
-      <div className='flex items-center justify-center w-6 h-6 bg-gray-200 border-1 border-gray-400 text-sm font-semibold'>
-        {this.state.type === 'mine' ? 'B' : this.state.value}
-      </div>
-    )
-  }
 }
 
 export default Cell;
