bc838336c6999fbb88715de5c1c0302913e673c8
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2023-01-31 01:07:19 +0100

    Added a functional grid based on the size

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,33 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import { config, Difficulty } from '../lib/config';
-import Cell, {CellType} from './Cell';
+import { randomIntFromInterval } from '../lib/functions';
+import Cell from './Cell';
 
-type BoardType = {
+type BoardProps = {
   difficulty: string
 }
 
-const Board = ({difficulty} : BoardType)  => {
-  const board = config[difficulty as Difficulty];
-  const [map, setMap] = useState<CellType>();
+const Board = ({difficulty} : BoardProps)  => {
+  const cfg = config[difficulty as Difficulty];
+  let board = new Array(cfg.width).fill(0).map(() => new Array(cfg.height).fill(1));
 
   useEffect(() => {
+    for(var i = 0; i < cfg.width; i++) {
+      for(var j = 0; j < cfg.height; j++) {
+        board[i][j] = new Cell({
+          isOpened: false,
+          type: 'empty',
+          value: -1,
+        });
+      }
+    }
+
     // Set bomb cells
-    let bombCounter = board.mines;
+    let bombCounter = cfg.mines;
 
     while(bombCounter) {
       // Set bomb to random coordinates of map
+      let x: number = randomIntFromInterval(0, cfg.width - 1);
+      let y: number = randomIntFromInterval(0, cfg.height - 1);
+      board[x][y] = new Cell({
+        isOpened: false,
+        type: 'mine',
+        value: -1,
+      });
       bombCounter--;
     }
 
     // Set empty cells
-  }, [board]);
+  }, [cfg]);
 
   return (
     <div>Board <br />
       <div>
-        {board.width} - {board.height} - {board.mines}
-        <Cell type='empty' value={2} />
-        <Cell type='mine' value={-1} />
+        {cfg.width} - {cfg.height} - {cfg.mines}
+        <div className='board flex flex-col'>
+        {board.map((cells, i) => {
+          return (
+            <div key={i} className='row flex flex-row'>
+              {cells.map((subCells, j) => {
+                return (
+                <div key={j} className='flex items-center justify-center w-6 h-6 bg-gray-200 border-1 border-gray-400 text-sm font-semibold'>
+                  <span></span>
+                </div>
+                )
+              })}
+            </div>
+          )
+        })}
+        </div>
       </div>
     </div>
   )
--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,27 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-export type CellType = {
+type CellState = {
   isOpened?: boolean,
   type: 'mine' | 'empty',
   value: number,
 }
 
-const Cell = ({type, value}: CellType) => {
-  const [data, setData] = useState<CellType>();
+class Cell extends React.Component<CellState> {
+  state: CellState = {
+    isOpened: false,
+    type: 'empty',
+    value: -1
+  }
 
-  useEffect(() => {
-    setData({
-      isOpened: false,
-      type: type,
-      value: value
-    });
-  }, []);
-
-  return (
-    <div className='flex items-center justify-center w-6 h-6 bg-gray-200 border-1 border-gray-400 text-sm font-semibold'>
-      {type === 'mine' ? 'B' : data?.value}
-    </div>
-  )
+  render() {
+    return (
+      <div className='flex items-center justify-center w-6 h-6 bg-gray-200 border-1 border-gray-400 text-sm font-semibold'>
+        {this.state.type === 'mine' ? 'B' : this.state.value}
+      </div>
+    )
+  }
 }
 
 export default Cell;
