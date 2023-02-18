e8b9cd07bc367342ec2fb115c89c7e97f06c0728
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2023-02-18 17:59:11 +0100

    Added a function for leading zeroes for the flag counter and timer

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,3 +1,7 @@
 export function randomIntFromInterval(min: number, max: number) { // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+export function leadingZeroes(n: number, len: number) {
+  return String(n).padStart(len, '0');
+}
